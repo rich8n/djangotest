875b9aa858dbf6efccaaadf8cb48b85b2aef4af9
Rich Aten <rich8n@gmail.com>
2018-06-05 15:54:40 -0600

    My Django Test App, first commit

---
+++ handicaps/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from .models import Golfer
+from .models import Team
+
+admin.site.register(Golfer)
+admin.site.register(Team)
+# Register your models here.
---
+++ handicaps/models.py
@@ -0,0 +1,33 @@
+from django.db import models
+from django.utils import timezone
+
+class Golfer(models.Model):
+    first = models.CharField(max_length=25)
+    last = models.CharField(max_length=25)
+    suffix = models.CharField(max_length=8, blank=True, null=True)
+    tees = models.CharField(max_length=1)
+    status = models.CharField(max_length=12)
+    team = models.ForeignKey('Team', on_delete=models.CASCADE)
+    team_role = models.CharField(max_length=12)
+    email = models.CharField(max_length=60, blank=True, null=True)
+    phone = models.CharField(max_length=10, blank=True, null=True)
+    created_date = models.DateTimeField(default=timezone.now)
+    updated_date = models.DateTimeField(default=timezone.now)
+
+    def update(self):
+        self.updated_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        if self.suffix == None:
+            suf = ''
+        else:
+            suf = f' {self.suffix}'
+        return f'{self.last}, {self.first}{suf}'
+
+class Team(models.Model):
+    name = models.CharField(max_length=40)
+    status = models.CharField(max_length=12)
+
+    def __str__(self):
+        return self.name
